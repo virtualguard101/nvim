1fe064ad7cba62b2d1041d3df481639ddfca0262
vg101 <virtualguard101@gmail.com>
2026-03-30 13:16:20 +0800

    config: update some keymap config

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -15,8 +15,8 @@ vim.opt.tabstop = 2;
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true;
 vim.opt.expandtab = true;
-vim.opt.number = true;
-vim.opt.relativenumber = true;
+vim.opt.number = true
+vim.opt.relativenumber = false
 
 vim.keymap.set('n',  '<leader>h',  ':nohlsearch<CR>');
 
